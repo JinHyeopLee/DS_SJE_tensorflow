2506eef58ebbb9c69ee979f92578d184e41a10e7
JinHyeopLee <jhlee7467@gmail.com>
2018-11-22 12:45:12 +0900

    20181121 update

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import argparse
-import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    # argument for path
+    parser.add_argument("--write_model_path", default="/media/jh/data/DS_SJE_model")
+    parser.add_argument("--train_img_path", default="/home/jh/CUB/images")
+    parser.add_argument("--train_txt_path", default="/home/jh/CUB/text_c10")
+    parser.add_argument("--train_meta_path", default="/home/jh/CUB/trainclasses.txt")
+    parser.add_argument("--valid_meta_path", default="/home/jh/CUB/valclasses.txt")
+    parser.add_argument("--test_meta_path", default="/home/jh/CUB/testclasses.txt")
+
+    # argument for text encoder's hyperparameter
+    parser.add_argument("--learning_rate", default=0.0007, type=float)
+    parser.add_argument("--alphabet_size", default=71, type=int)
+    parser.add_argument("--maximum_text_length", default=201, type=int)
+    parser.add_argument("--cnn_represent_dim", default=1024, type=int)
+
+    # argument for data loader
+    parser.add_argument("--train_img_data_type", default="*.npy")
+    parser.add_argument("--train_txt_data_type", default="*.txt")
+    parser.add_argument("--length_char_string", default=201, type=int)
+    parser.add_argument("--multi_process_num_thread", default=12, type=int)
+
+    args = parser.parse_args()
+
+    # put more code!
---
+++ model.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+
+
+class DS_SJE_model():
+    def __init__(self, **args):
+        self.args = args['args']
+        print(self.args)
+
+
+    def DS_SJE(self, text_input, is_train=True):
+        conv1 = tf.nn.conv2d(input=text_input,
+                             filter=[4, 1, self.args.alphabet_size, 384],
+                             strides=[1, 1, 1, 1],
+                             padding='VALID') # 198 x 1 x 384 dimension
+        conv1_activation = tf.nn.relu(conv1)
+        conv1_max_pool = tf.nn.max_pool(value=conv1_activation,
+                                        ksize=[1, 3, 1, 1],
+                                        strides=[1, 3, 1, 1],
+                                        padding='VALID') # 66 x 1 x 384 dimension
+
+        conv2 = tf.nn.conv2d(input=conv1_max_pool,
+                             filter=[4, 1, 384, 512],
+                             strides=[1, 1, 1, 1],
+                             padding='VALID') # 63 x 1 x 512 dimension
+        conv2_activation = tf.nn.relu(conv2)
+        conv2_max_pool = tf.nn.max_pool(value=conv2_activation,
+                                        ksize=[1, 3, 1, 1],
+                                        strides=[1, 3, 1, 1],
+                                        padding='VALID') # 21 x 1 x 512 dimension
+
+        conv3 = tf.nn.conv2d(input=conv2_max_pool,
+                             filter=[5, 1, 512, self.args.cnn_represent_dim],
+                             strides=[1, 1, 1, 1],
+                             padding='VALID') # 18 x 1 x 1024 dimension
+        conv3_activation = tf.nn.relu(conv3)
+        conv3_max_pool = tf.nn.max_pool(value=conv3_activation,
+                                        ksize=[1, 3, 1, 1],
+                                        strides=[1, 2, 1, 1],
+                                        padding='VALID')
+
+        rnn_cell = tf.nn.rnn_cell.BasicRNNCell(self.args.cnn_represent_dim)
+        outputs, state = tf.nn.static_rnn(rnn_cell, conv3_max_pool)
+
+        embedded_code = tf.reduce_mean(state, axis=0)
+
+        print('DS_SJE Architecture')
+        print(np.shape(conv1_activation))
+        print(np.shape(conv1_max_pool))
+        print(np.shape(conv2_activation))
+        print(np.shape(conv2_max_pool))
+        print(np.shape(conv3_activation))
+        print(np.shape(conv3_max_pool))
+        print(np.shape(embedded_code))
+
+        return embedded_code
